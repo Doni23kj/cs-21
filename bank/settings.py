747f472bd03b384e09800a1181ea7fba94ef00ec
Doni23kj <https://github.com/Doni23kj/Customer-Segmentation-Project.git>
2026-04-07 16:27:11 +0600

    update settings

--- bank/settings.py
+++ bank/settings.py
@@ -81,17 +81,22 @@ WSGI_APPLICATION = 'bank.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres.lusgkfwfrfbdtldlcaqs',
+#         'PASSWORD': 'nqpujdtjzpejyemkjzhv',
+#         'HOST': 'aws-1-ap-south-1.pooler.supabase.com',
+#         'PORT': '6543',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres.lusgkfwfrfbdtldlcaqs',
-        'PASSWORD': 'nqpujdtjzpejyemkjzhv',
-        'HOST': 'aws-1-ap-south-1.pooler.supabase.com',
-        'PORT': '6543',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
